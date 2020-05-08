98d24163c4b588c7750dd6b5a8902b9b6bab9d5b
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-05-08 21:32:55 +0300

    refactoring

--- invoiceGenarator/index.php
+++ invoiceGenarator/index.php
@@ -23,27 +23,32 @@ $connection = new createCon();
 $connection->connect();
 
 
-$invoiceSelect = 'SELECT * FROM  `invoice_Information`';
 
+//BILL INFO
+$invoiceSelect = 'SELECT * FROM  `invoice_Information`';
 $result = mysqli_query($connection->myConn, $invoiceSelect);
 $invInfo = mysqli_fetch_assoc($result);;
-
-$billAndShipSelect = 'SELECT * FROM  `ship_to`'; //BILL AND SHIP INFO
-
-$resultBill = mysqli_query($connection->myConn, $billAndShipSelect);
-$shipAndBill = mysqli_fetch_assoc($resultBill);
-
 //BILL TO
 $invoice->set("billto",$invInfo);
 
+//SHIP INFO
+$shipSelect = 'SELECT * FROM  `ship_to`';
+
+$resultBill = mysqli_query($connection->myConn, $shipSelect);
+$shipTo = mysqli_fetch_assoc($resultBill);
+
+
+
 //SHIP TO
-$invoice->set("shipto", $shipAndBill);
+$invoice->set("shipto", $shipTo);
 
 $selectCart = 'SELECT * FROM `item_details`';
 $itemResult = mysqli_query($connection->myConn,$selectCart);
 
 $totalSum = floatval(0);
 
+
+//FETCHING ITEMS FROM DB
 $items = [];
     while($row = $itemResult->fetch_assoc()) {
 
@@ -58,18 +63,19 @@ $items = [];
 foreach ($items as $i) {
     $invoice->add("items", $i); }
 
-//TOTALS
+//TOTALS INFO
 $invoice->set("totals",[
     ["СУМА", $totalSum . "лв."]
 ]);
 
 
 
-
+//SUB INFO
 $invoice->set("notes", [
     "Благодарим ви че сте наш клиент."
 ]);
 
+//SHOWING INVOICE.HTML
 $invoice->outputHTML();
 
 if (isset($_POST['test'])) {
