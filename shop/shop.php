61f89f40949bf21ec28f8663014c9288d43c88dc
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-06-17 10:29:17 +0300

    Update shop.php

--- shop/shop.php
+++ shop/shop.php
@@ -60,11 +60,8 @@
 
 <?php
 $link = mysqli_connect("127.0.0.1", "root", "", "shop");
-
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
 
